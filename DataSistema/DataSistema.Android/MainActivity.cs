c8d0fde8028040b19aae5d695906b0310f0f9aec
Christian Picado Mesen <cpicado2610@gmail.com>
2018-07-30 15:51:15 -0600

    nuevo

--- DataSistema/DataSistema.Android/MainActivity.cs
+++ DataSistema/DataSistema.Android/MainActivity.cs
@@ -20,7 +20,8 @@ namespace DataSistema.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+            LoadApplication(new App()
+                );
         }
     }
 }
